b469f0afd94060953faacff1fd94db786e0750e3
huangjue <huangjue@meituan.com>
2022-01-04 10:56:43 +0800

    更新文件

--- js/allMembers.js
+++ js/allMembers.js
@@ -1,286 +1,134 @@
 var allMembers = [
   {
-    phone: "No.1",
-    name: "zhoudunyang",
+    phone: 'No.1',
+    name: '骆晶明',
   },
   {
-    phone: "No.2",
-    name: "huangjue",
+    phone: 'No.2',
+    name: '高菲菲',
   },
   {
-    phone: "No.3",
-    name: "liujiang04",
+    phone: 'No.3',
+    name: '康琪',
   },
   {
-    phone: "No.4",
-    name: "zhumengzhu",
+    phone: 'No.4',
+    name: '李静',
   },
   {
-    phone: "No.5",
-    name: "huozhiying",
+    phone: 'No.5',
+    name: '刘静',
   },
   {
-    phone: "No.6",
-    name: "zhangzhe15",
+    phone: 'No.6',
+    name: '陈静',
   },
   {
-    phone: "No.7",
-    name: "wangtong09",
+    phone: 'No.7',
+    name: '夏美玲',
   },
   {
-    phone: "No.8",
-    name: "shihuashen",
+    phone: 'No.8',
+    name: '宋亚萍',
   },
   {
-    phone: "No.9",
-    name: "wangzhicheng03",
+    phone: 'No.9',
+    name: '冯秀娟',
   },
   {
-    phone: "No.10",
-    name: "liuyanghe02",
+    phone: 'No.10',
+    name: '唐晨',
   },
   {
-    phone: "No.11",
-    name: "yangzhongjing",
+    phone: 'No.11',
+    name: '邱敏欣',
   },
   {
-    phone: "No.12",
-    name: "peiyu02",
+    phone: 'No.12',
+    name: '杨云',
   },
   {
-    phone: "No.13",
-    name: "zhidi.xia",
+    phone: 'No.13',
+    name: '王青',
   },
   {
-    phone: "No.14",
-    name: "zhangchen29",
+    phone: 'No.14',
+    name: '王馨悦',
   },
   {
-    phone: "No.15",
-    name: "liteng07",
+    phone: 'No.15',
+    name: '芦丹',
   },
   {
-    phone: "No.16",
-    name: "liruifeng",
+    phone: 'No.16',
+    name: '李静雯',
   },
   {
-    phone: "No.17",
-    name: "zhouchangshun",
+    phone: 'No.17',
+    name: '徐亭亭',
   },
   {
-    phone: "No.18",
-    name: "qiushi02",
+    phone: 'No.18',
+    name: '杜祝英',
   },
   {
-    phone: "No.19",
-    name: "yangguoliang",
+    phone: 'No.19',
+    name: '董旭冉',
   },
   {
-    phone: "No.20",
-    name: "wangpeng42",
+    phone: 'No.20',
+    name: '杨露',
   },
   {
-    phone: "No.21",
-    name: "zhangxianpeng",
+    phone: 'No.21',
+    name: '孟瑞雪',
   },
   {
-    phone: "No.22",
-    name: "wangyafei07",
+    phone: 'No.22',
+    name: '孙金玥',
   },
   {
-    phone: "No.23",
-    name: "yangliuning",
+    phone: 'No.23',
+    name: '刘素素',
   },
   {
-    phone: "No.24",
-    name: "hante",
+    phone: 'No.24',
+    name: '钟国伟',
   },
   {
-    phone: "No.25",
-    name: "xusuxuan",
+    phone: 'No.25',
+    name: '杨喆',
   },
   {
-    phone: "No.26",
-    name: "maiming",
+    phone: 'No.26',
+    name: '王瑶',
   },
   {
-    phone: "No.27",
-    name: "changjing03",
+    phone: 'No.27',
+    name: '张莹',
   },
   {
-    phone: "No.28",
-    name: "qinzhanqiang",
+    phone: 'No.28',
+    name: '郭瑞',
   },
   {
-    phone: "No.29",
-    name: "yangjingwen05",
+    phone: 'No.29',
+    name: '白力丞',
   },
   {
-    phone: "No.30",
-    name: "yangyidong02",
+    phone: 'No.30',
+    name: '胡学艺',
   },
   {
-    phone: "No.31",
-    name: "zhangyuchen04",
+    phone: 'No.31',
+    name: '韩月宾',
   },
   {
-    phone: "No.32",
-    name: "xutaokai",
+    phone: 'No.32',
+    name: '朱洁',
   },
   {
-    phone: "No.33",
-    name: "zhaohailong06",
-  },
-  {
-    phone: "No.34",
-    name: "suxun",
-  },
-  {
-    phone: "No.35",
-    name: "zhangtengteng03",
-  },
-  {
-    phone: "No.36",
-    name: "caozhengwei",
-  },
-  {
-    phone: "No.37",
-    name: "zhangxilin02",
-  },
-  {
-    phone: "No.38",
-    name: "liuhenglin",
-  },
-  {
-    phone: "No.39",
-    name: "litao63",
-  },
-  {
-    phone: "No.40",
-    name: "chenhaitao04",
-  },
-  {
-    phone: "No.41",
-    name: "qujingguo",
-  },
-  {
-    phone: "No.42",
-    name: "liyuxin06",
-  },
-  {
-    phone: "No.43",
-    name: "zhaohuiying02",
-  },
-  {
-    phone: "No.44",
-    name: "chenxiang25",
-  },
-  {
-    phone: "No.45",
-    name: "gaozhong03",
-  },
-  {
-    phone: "No.46",
-    name: "houzhiyuan02",
-  },
-  {
-    phone: "No.47",
-    name: "wb_suzhehui",
-  },
-  {
-    phone: "No.48",
-    name: "xujinkang02",
-  },
-  {
-    phone: "No.49",
-    name: "zhouzhe04",
-  },
-  {
-    phone: "No.50",
-    name: "maye04",
-  },
-  {
-    phone: "No.51",
-    name: "qinwenchao02",
-  },
-  {
-    phone: "No.52",
-    name: "wb_wangshenbing",
-  },
-  {
-    phone: "No.53",
-    name: "shenzhili",
-  },
-  {
-    phone: "No.54",
-    name: "fangxiaoming",
-  },
-  {
-    phone: "No.55",
-    name: "zhangyusheng05",
-  },
-  {
-    phone: "No.56",
-    name: "chenjian45",
-  },
-  {
-    phone: "No.57",
-    name: "yangwenqing",
-  },
-  {
-    phone: "No.58",
-    name: "guohuang",
-  },
-  {
-    phone: "No.59",
-    name: "hukaidi",
-  },
-  {
-    phone: "No.60",
-    name: "tongxiaotian",
-  },
-  {
-    phone: "No.61",
-    name: "sunsongtao",
-  },
-  {
-    phone: "No.62",
-    name: "liukaiyuan",
-  },
-  {
-    phone: "No.63",
-    name: "nichengyu",
-  },
-  {
-    phone: "No.64",
-    name: "yaodan03",
-  },
-  {
-    phone: "No.65",
-    name: "wudijia",
-  },
-  {
-    phone: "No.66",
-    name: "sunqi24",
-  },
-  {
-    phone: "No.67",
-    name: "liangyuanhao",
-  },
-  {
-    phone: "No.68",
-    name: "zhanghuaxiang",
-  },
-  {
-    phone: "No.69",
-    name: "chenhualei",
-  },
-  {
-    phone: "No.70",
-    name: "mijing02",
-  },
-  {
-    phone: "No.71",
-    name: "yangfei34",
+    phone: 'No.33',
+    name: '鲁萍',
   },
 ];
